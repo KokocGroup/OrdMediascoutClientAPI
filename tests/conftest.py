4260b93a9d0fad7c3f86b0b67cb9157041477d9d
Alexey Ruzin <ruzin@kokoc.com>
2024-01-11 01:54:33 +0300

    fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,6 @@ from ord_mediascout_client import ORDMediascoutClient, ORDMediascoutConfig
 
 
 @pytest.fixture
-def client() -> ORDMediascoutClient:
+def client():
     config = ORDMediascoutConfig()
     return ORDMediascoutClient(config)
