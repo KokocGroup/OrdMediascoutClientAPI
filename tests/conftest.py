e0f9eedd488711c04d511c042abdb94a4a4694e2
Alexey Ruzin <ruzin@kokoc.com>
2024-01-12 02:58:50 +0300

    create tests on feeds

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import time
+
 import pytest
 
 from ord_mediascout_client import ORDMediascoutClient, ORDMediascoutConfig
@@ -7,3 +9,13 @@ from ord_mediascout_client import ORDMediascoutClient, ORDMediascoutConfig
 def client():
     config = ORDMediascoutConfig()
     return ORDMediascoutClient(config)
+
+
+@pytest.fixture(scope='session', autouse=True)
+def faker_session_locale():
+    return ['ru_RU']
+
+
+@pytest.fixture(scope='session', autouse=True)
+def faker_seed():
+    return int(time.time())
