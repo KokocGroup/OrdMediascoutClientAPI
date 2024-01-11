e1fc15fdf8206a6d15888230d80ef7febc592a66
Alexey Ruzin <ruzin@kokoc.com>
2024-01-12 02:59:28 +0300

    example script for working with feeds

--- examples/create_feed.py
+++ examples/create_feed.py
@@ -1,46 +1,126 @@
+import os
+
+from dotenv import load_dotenv
+
 from ord_mediascout_client import (
-    ClientRelationshipType,
-    CreateClientWebApiDto,
-    GetClientsWebApiDto,
-    LegalForm,
+    CreateFeedElementsBulkWebApiDto,
+    CreateFeedElementsWebApiDto,
+    FeedElementTextDataItem,
+    FeedElementWebApiDto,
+    GetFeedElementsBulkInfo,
+    GetFeedElementsWebApiDto,
     ORDMediascoutClient,
     ORDMediascoutConfig,
 )
-from ord_mediascout_client.client import APIError, BadResponseError, UnexpectedResponseError
 
-config = ORDMediascoutConfig(url='http://localhost:5000', username='username', password='password')
+load_dotenv()
 
-api = ORDMediascoutClient(config)
+if os.getenv('LOGGING') == 'debug':
+    import logging
 
-client = CreateClientWebApiDto(
-    createMode=ClientRelationshipType.DirectClient,
-    legalForm=LegalForm.JuridicalPerson,
-    inn='7702070139',
-    name='Test Client',
-    mobilePhone='1234567890',
-    epayNumber=None,
-    regNumber=None,
-    oksmNumber=None,
+    logger = logging.getLogger('ord_mediascout_client')
+    logger.setLevel(logging.DEBUG)
+    logging.basicConfig(level=logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    handler = logging.FileHandler('ord_mediascout_client.log')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+
+api = ORDMediascoutClient(ORDMediascoutConfig())
+
+
+#  Создаем несколько элементов в новом фиде
+create_feed_elements__request_dto = CreateFeedElementsWebApiDto(
+    feedName='example',
+    feedNativeCustomerId='example_id',
+    feedElements=[
+        FeedElementWebApiDto(
+            nativeCustomerId='7',
+            description='haha7',
+            advertiserUrls=['https://site.ru'],
+            textData=[FeedElementTextDataItem(textData='haha')],
+        ),
+        FeedElementWebApiDto(
+            description='haha8',
+            nativeCustomerId='8',
+            advertiserUrls=['https://site.ru'],
+            textData=[FeedElementTextDataItem(textData='haha')],
+        ),
+        FeedElementWebApiDto(
+            description='haha9',
+            nativeCustomerId='9',
+            advertiserUrls=['https://site.ru'],
+            textData=[FeedElementTextDataItem(textData='haha')],
+        ),
+    ],
 )
+create_feed_elements__response_dto = api.create_feed_elements(create_feed_elements__request_dto)
+print(create_feed_elements__response_dto)
 
-try:
-    client = api.create_client(client)
-    print(client)
 
-    clients = api.get_clients(GetClientsWebApiDto())
-    for client in clients:
-        print(client)
+# Создаем несколько элементов в ранее созданном фиде
+append_feed_elements__request_dto = CreateFeedElementsWebApiDto(
+    feedId='FDZpYSRVLhQEG0p-x53GW2BA',
+    feedElements=[
+        FeedElementWebApiDto(
+            nativeCustomerId='17',
+            description='haha17',
+            advertiserUrls=['https://site.ru'],
+            textData=[FeedElementTextDataItem(textData='haha')],
+        ),
+        FeedElementWebApiDto(
+            description='haha18',
+            nativeCustomerId='18',
+            advertiserUrls=['https://site.ru'],
+            textData=[FeedElementTextDataItem(textData='haha')],
+        ),
+        FeedElementWebApiDto(
+            description='haha19',
+            nativeCustomerId='19',
+            advertiserUrls=['https://site.ru'],
+            textData=[FeedElementTextDataItem(textData='haha')],
+        ),
+    ],
+)
+append_feed_elements__response_dto = api.create_feed_elements(append_feed_elements__request_dto)
+print(append_feed_elements__response_dto)
 
-except UnexpectedResponseError as ex:
-    print('UnexpectedResponseError', ex)
-    print('request:', ex.response.request.body)
-    print('response:', ex.response.text)
-except BadResponseError as ex:
-    print('BadRequestError', ex)
-    for er in ex.error.errorItems:
-        print('  error:', er)
-except APIError as ex:
-    print('APIError', ex)
-except Exception as ex:
-    print('Exception', type(ex), ex)
-    raise
+#  Получаем список элементов фида по ID элементов
+dto = GetFeedElementsWebApiDto(ids=['11375804f', '11375803f', '11375804f', '11375803f'])
+result_dto = api.get_feed_elements(dto)
+
+
+# Создаем пачку элементов в ранее созданном фиде (НЕ РАБОТАЕТ)
+create_feed_elements_bulk__request_dto = CreateFeedElementsBulkWebApiDto(
+    feedElements=[
+        FeedElementWebApiDto(
+            feedId='FDwwmDToQk_ES1GGezF9iH1g',
+            nativeCustomerId='7',
+            description='haha7',
+            advertiserUrls=['https://site.ru'],
+            textData=[FeedElementTextDataItem(textData='haha')],
+        ),
+        FeedElementWebApiDto(
+            feedId='FDwwmDToQk_ES1GGezF9iH1g',
+            description='haha8',
+            nativeCustomerId='8',
+            advertiserUrls=['https://site.ru'],
+            textData=[FeedElementTextDataItem(textData='haha')],
+        ),
+        FeedElementWebApiDto(
+            feedId='FDwwmDToQk_ES1GGezF9iH1g',
+            description='haha9',
+            nativeCustomerId='9',
+            advertiserUrls=['https://site.ru'],
+            textData=[FeedElementTextDataItem(textData='haha')],
+        ),
+    ]
+)
+create_feed_elements_bulk__response_dto = api.create_feed_elements_bulk(create_feed_elements_bulk__request_dto)
+print(create_feed_elements_bulk__response_dto)
+
+#  Получаем информацию о пачке с id = 'EB_LT_17n4h0-Bec4pKyhOFw'
+dto = GetFeedElementsBulkInfo(id='EB_LT_17n4h0-Bec4pKyhOFw')
+result_dto = api.get_feed_elements_bulk_info(dto)
+print(result_dto)
